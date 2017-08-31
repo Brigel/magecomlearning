8e2209c54af1ae1119693e8a1d6c8f78110db6c5
Unknown <kostyasolovichenko@mail.ru>
2017-08-31 18:36:52 +0300

    Start work with brand
    Not finished

---
+++ app/code/Tasks/Brand/Model/ResourceModel/Brand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tasks\Brand\Model\ResourceModel;
+
+use Magento\Catalog\Model\ResourceModel\AbstractResource;
+
+/**
+ * Brand entity resource model
+ *
+ * @SuppressWarnings(PHPMD.LongVariable)
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Brand extends AbstractResource
+{
+    /**
+     * Entity type getter and lazy loader
+     *
+     * @return \Magento\Eav\Model\Entity\Type
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getEntityType()
+    {
+        if (empty($this->_type)) {
+            $this->setType(\Tasks\Brand\Model\Brand::ENTITY);
+        }
+        return parent::getEntityType();
+    }
+}
---
+++ app/code/Tasks/Brand/Model/ResourceModel/Brand/Collection.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tasks\Brand\Model\ResourceModel\Brand;
+
+/**
+ * Brand resource collection
+ *
+ */
+class Collection extends \Magento\Catalog\Model\ResourceModel\Collection\AbstractCollection
+{
+    /**
+     * Brand collection factory
+     *
+     * @var \Tasks\Brand\Model\ResourceModel\Brand\CollectionFactory
+     */
+    protected $_brandCollectionFactory;
+
+    /**
+     * @param \Magento\Framework\Data\Collection\EntityFactory $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Eav\Model\Config $eavConfig
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param \Magento\Eav\Model\EntityFactory $eavEntityFactory
+     * @param \Magento\Catalog\Model\ResourceModel\Helper $resourceHelper
+     * @param \Magento\Framework\Validator\UniversalFactory $universalFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Tasks\Brand\Model\ResourceModel\Brand\CollectionFactory $brandCollectionFactory
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactory $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Eav\Model\Config $eavConfig,
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Eav\Model\EntityFactory $eavEntityFactory,
+        \Magento\Catalog\Model\ResourceModel\Helper $resourceHelper,
+        \Magento\Framework\Validator\UniversalFactory $universalFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Tasks\Brand\Model\ResourceModel\Brand\CollectionFactory $brandCollectionFactory,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null
+    ) {
+        $this->_brandCollectionFactory = $brandCollectionFactory;
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $eavConfig,
+            $resource,
+            $eavEntityFactory,
+            $resourceHelper,
+            $universalFactory,
+            $storeManager,
+            $connection
+        );
+    }
+
+    /**
+     * Init collection and determine table names.
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Tasks\Brand\Model\Brand', 'Tasks\Brand\Model\ResourceModel\Brand');
+    }
+
+    /**
+     * Convert items array to array for select options.
+     *
+     * @param $addEmpty bool
+     * @return array
+     */
+    public function toOptionArray($addEmpty = true)
+    {
+        /** @var \Tasks\Brand\Model\ResourceModel\Brand\Collection $collection */
+        $collection = $this->_brandCollectionFactory->create();
+
+        $collection->addAttributeToSelect('name')->load();
+
+        $options = [];
+
+        if ($addEmpty) {
+            $options[] = ['label' => __('-- Please Select a Brand --'), 'value' => ''];
+        }
+        foreach ($collection as $brand) {
+            $options[] = ['label' => $brand->getName(), 'value' => $brand->getId()];
+        }
+
+        return $options;
+    }
+}
